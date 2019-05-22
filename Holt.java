0362896afa10e8942d368a8fdd0359e3e57891c4
Bernardo T. Abreu <berTrindade@users.noreply.github.com>
2019-05-22 20:08:36 -0300

    Add files via upload

---
+++ Holt.java
@@ -0,0 +1,66 @@
+public class HoltProcess extends SkeletonProcess {
+
+    private final Image resultImage;
+    private Image processImage;
+
+    public HoltProcess(Image image) {
+        super(image);
+        this.resultImage = ImageFactory.buildEmptyImage(image);
+        this.processImage = new Image(image);
+        setFinalizer(() -> {
+            setOutput(resultImage);
+        });
+    }
+    
+    /**
+     * calculate the pixel value
+     *
+     * @param pixels
+     * @param first
+     * @return int
+     */
+    public int calc(int[][] pixels, boolean first) {
+        int[] neighborhood = neighborhood(pixels);
+        if (!isEdge(neighborhood)) {
+            return pixels[1][1];
+        }
+        int n = pixels[1][0];
+        int l = pixels[2][1];
+        int s = pixels[1][2];
+        int o = pixels[0][1];
+        if (first) {
+            if (isHigher(l) && isHigher(s) && (isHigher(n) || isHigher(o))) {
+                return pixels[1][1];
+            }
+        } else {
+            if (isHigher(o) && isHigher(n) && (isHigher(s) || isHigher(l))) {
+                return pixels[1][1];
+            }
+        }
+        return 0;
+    }
+
+    @Override
+    protected void processImage() {
+        boolean change = true;
+        boolean firstStep = false;
+        while (change) {
+            change = false;
+            firstStep = !firstStep;
+            for (int x = 1; x < processImage.getWidth() - 1; x++) {
+                for (int y = 1; y < processImage.getHeight() - 1; y++) {
+                    if (processImage.get(0, x, y) == image.getPixelValueRange().getHigher()) {
+                        int[][] pixels = pixels(x, y, processImage);
+                        int v = Math.max(Math.min(calc(pixels, firstStep), image.getPixelValueRange().getHigher()), 0);
+                        if (v != processImage.get(0, x, y)) {
+                            change = true;
+                        }
+                        resultImage.set(0, x, y, v);
+                    }
+                }
+            }
+            processImage = new Image(resultImage);
+        }
+    }
+
+}
